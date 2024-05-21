1b7b9f5068730002f09c3e7c06b17a7409ae6a5b
hayone1 <boluwatife.morawo@stu.cu.edu.ng>
2024-05-21 20:03:49 +0100

    working

--- infra-template/digital_ocean/infra.tf
+++ infra-template/digital_ocean/infra.tf
@@ -85,11 +85,11 @@ resource "ssh_resource" "hosts-data" {
         index=$(echo -n ${digitalocean_droplet.microcloud-droplet[count.index].name} | tail -c 1)  &&
         hostname=$(hostname) &&
         ipv4_address=$(hostname -I | awk '{print $1}') &&
+        ipv4_address_iface=$(ip -o addr show | grep "inet $ipv4_address" | awk '{print $2}') &&
         ipv4_address_private='${digitalocean_droplet.microcloud-droplet[count.index].ipv4_address_private}' &&
-        private_interface='${digitalocean_droplet.microcloud-droplet[count.index].ipv4_address_private}' &&
+        primary_interface='${digitalocean_droplet.microcloud-droplet[count.index].ipv4_address_private}' &&
         ipv4_address_private_cidr=$(ip -o addr show | grep "inet $ipv4_address_private" | awk '{print $4}') &&
-        ipv4_address_private_iface=$(ip -o addr show | grep "inet $ipv4_address_private" | awk '{print $2}') &&
-        echo -e "hostname: $hostname\nipv4_address: $ipv4_address\nipv4_address_private: $ipv4_address_private\nindex_key: $index\nlocal_volume_path: $local_volume_path\nceph_volume_path: $ceph_volume_path\nipv4_address_private_cidr: $ipv4_address_private_cidr\n"
+        echo -e "hostname: $hostname\nipv4_address: $ipv4_address\nipv4_address_iface: $ipv4_address_iface\nipv4_address_private: $ipv4_address_private\nindex_key: $index\nlocal_volume_path: $local_volume_path\nceph_volume_path: $ceph_volume_path\nipv4_address_private_cidr: $ipv4_address_private_cidr\n"
     EOF
   ]
 }
@@ -104,14 +104,6 @@ output "ansible-hosts" {
 
   }
 }
-
-# output "ansible-hosts" {
-#   value = {
-#     for volume_datum in out :
-#       volume_datum.index_key => volume_datum
-#   }
-# }
-
 
 # resource "digitalocean_loadbalancer" "microcloud_lb" {
 #   name    = "${local.prefix}-lb"
